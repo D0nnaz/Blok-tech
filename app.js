f6d70a219b60755c3fe966d690519ac9d293b991
D0nnaz <stambassadeur@gmail.com>
2023-04-25 14:19:24 +0200

    g

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const PORT = process.env.port || 1337;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', 'view');
+app.set('views', 'views');
 
 
 app.get('/', (req, res) => {
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${port}`)
+  console.log(`Server running on port: ${PORT}`)
 });
