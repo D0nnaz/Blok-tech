96b249eaf9fd5ac3126d5b5a03a03ddf935a54ed
D0nnaz <stambassadeur@gmail.com>
2023-05-16 15:30:35 +0200

    history

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-const database = client.db("chatlingo");
-const messagesDB = database.collection("messages");
-console.log("MONGODB IS HIER YUH :)");
+// const database = client.db("chatlingo");
+// const messagesDB = database.collection("messages");
+// console.log("MONGODB IS HIER YUH :)");
 
-const messagesCollection = client.db().collection("messages");
+// const messagesCollection = client.db().collection("messages");
 
-const messages = [];
+// const messages = [];
 
-io.on("connection", async (socket) => {
-  console.log(`User ${socket.id} connected`);
+// io.on("connection", async (socket) => {
+//   console.log(`User ${socket.id} connected`);
 
-  messages.forEach((message) => {
-    socket.emit("message", message);
-  });
+//   messages.forEach((message) => {
+//     socket.emit("message", message);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log(`User ${socket.id} disconnected`);
-  });
+//   socket.on("disconnect", () => {
+//     console.log(`User ${socket.id} disconnected`);
+//   });
 
-  socket.on("message", async (msg) => {
-    msg.sender = socket.id;
-    await messagesCollection.insertOne(msg);
-    messages.push(msg);
-    io.emit("message", msg);
-    console.log(`Message toegevoegd aan MongoDB: ${msg}`);
-  });
-});
+//   socket.on("message", async (msg) => {
+//     msg.sender = socket.id;
+//     await messagesCollection.insertOne(msg);
+//     messages.push(msg);
+//     io.emit("message", msg);
+//     console.log(`Message toegevoegd aan MongoDB: ${msg}`);
+//   });
+// });
